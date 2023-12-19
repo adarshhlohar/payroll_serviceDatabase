8f57aaa41a02363d6e1552cbb5ee7858c47f95b4
Adarsh Hari Lohar <117507791+adarshhlohar@users.noreply.github.com>
2023-12-19 18:03:56 +0530

    UC-2  Created a table of payroll_Employee

--- payrollQueries.sql
+++ payrollQueries.sql
@@ -1,3 +1,12 @@
 create database payroll_service;
 show databases;
 use payroll_service;
+
+use payroll_service;
+Create Table employee_payroll(
+	id int not null primary key auto_increment,
+    name varchar(255) NOT NULL,
+    salary int NOT NULL,
+    start_date DATE NOT NULL
+);
+
